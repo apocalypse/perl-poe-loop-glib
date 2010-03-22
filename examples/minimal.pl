79d189c3cab2b13a42b5342d55c033716ca40c40
Apocalypse <perl@0ne.us>
2010-03-21 21:07:41 -0700

    tweak example scripts and add Gtk2_Counter.pl from POE Cookbook

--- examples/minimal.pl
+++ examples/minimal.pl
@@ -1,12 +1,13 @@
 # the only interesting thing here is that we load Glib first, so POE
 # uses the right loop.
+use strict; use warnings;
 
 use Glib;
 #use POE qw(Loop::Glib); # this is another way to specify which loop to use
 #use POE::Kernel { loop => 'Glib' }; # and yet another way.
 use POE; # let POE auto-detect the loop
 
-my $s => POE::Session->create (
+POE::Session->create (
 	inline_states => {
 		_start => sub {
 			$_[KERNEL]->yield('foo');
